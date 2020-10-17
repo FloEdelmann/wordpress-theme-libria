b0383fb99143ed8934ef0207f5a66a695f9da1b4
Flo Edelmann <florian-edelmann@online.de>
2020-10-17 18:26:12 +0200

    Change Google Plus icon to Instagram icon

---
+++ export-sprites.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+inkscape --export-dpi=96 --export-type=png --export-filename=sprites.png sprites.svg
+inkscape --export-dpi=192 --export-type=png --export-filename=sprites-highres.png sprites.svg
