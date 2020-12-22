7b1c6e1444593107df50f99beee9edbf47e55389
Flo Edelmann <florian-edelmann@online.de>
2020-12-22 09:07:45 +0100

    Fix typo and update wording in 404 page

--- 404.php
+++ 404.php
@@ -2,7 +2,7 @@
 <article id="post-0" class="post error404 not-found">
   <h1 class="entry-title">Seite nicht gefunden</h1>
   <section class="entry-content">
-    <p>Entschuldigung, aber diese Seite existiert nicht. Sie können das Suchformular benutzen, wenn Sie etwas betimmtes finden möchten. Ansonsten stöbern Sie einfach über das obige Menü auf unseren Seiten.</p>
+    <p>Entschuldigung, diese Seite existiert nicht. Sie können das Suchformular benutzen, wenn Sie etwas bestimmtes finden möchten. Ansonsten stöbern Sie einfach über das obige Menü auf unseren Seiten.</p>
     <?php get_search_form(); ?>
   </section>
 </article>
